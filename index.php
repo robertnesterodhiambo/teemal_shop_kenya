afbfc5e00436d4a7204ee2c668c8bd159944cac5
robert nester odhiambo <robertnesterodhiambo@gmail.com>
2023-08-25 16:45:48 +0300

    Static homepage

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>teemal tyre shop</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <!--bootstrap css link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- fonts link-->
@@ -12,7 +12,7 @@
 </head>
 <body>
     <!-- DIV Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
   <div class="container-fluid">
    <img src="images/logo.png" alt = "" class = "logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,6 +25,9 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Register</a>
@@ -43,6 +46,172 @@
     </div>
   </div>
 </nav>
+<!-- second child -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<ul class="navbar-nav me-auto">
+<li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>       
+</ul>
+</nav>
+
+<!-- third child -->
+<div class="bg-warning">
+    <h3 class="text-center">TEEMAL STORE</h3> <!-- HIDEN STORE -->
+    <P class="text-center">
+        Garage is a vehicles hospital pick only teemal
+    </P>
+</div>
+
+<!-- fourth child -->
+<div class="row">
+    <div class="col-md-10">
+        <!-- products` -->
+       <div class="row">
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/bridgestone-sport-rft.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">255/50R19 BRIDGESTONE DUELLER H/P SPORT RUN-FLAT</h5>
+    <p class="card-text">Looking for 255/50R19 Bridgestone Dueler H/P Sport Run-Flat tires in Kenya? 
+        Experience high-performance with enhanced safety. These run-flat tires provide security in case of punctures. 
+        Ensure compatibility and consider professional guidance for optimal results. Elevate your driving with advanced tire technology.
+         Get yours now!</p>
+    <a href="#" class="btn btn-warning">Add to cart</a>
+    <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/BF Goodrich Trail Terrain T 26560R18 110T Tire.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BF Goodrich Trail Terrain T 26560R18 110T Tire</h5>
+    <p class="card-text">Looking for BF Goodrich Trail Terrain T 265/60R18 110T tires in Kenya? 
+        Experience all-terrain excellence with durability and performance. Ideal for diverse conditions.
+         Prioritize compatibility and regular maintenance for optimal results. Find your tires today!</p>
+    <a href="#" class="btn btn-warning">Add to cart</a> 
+    <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/BFGoodrich-Terrain-T-A-KO2-Radial-tire-LT265-60R18-E-119-116S-119S.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">BFGoodrich-Terrain-T-A-KO2-Radial-tire-LT265-60R18-E-119-116S-119S</h5>
+    <p class="card-text">Looking for BFGoodrich Terrain T/A KO2 Radial tire LT265/60R18 E 119/116S (119S) in Kenya? 
+        Discover excellent all-terrain performance with durability. Perfect for diverse conditions.
+         Prioritize compatibility and maintenance for optimal results. Get your tires now!</p>
+         <a href="#" class="btn  btn-warning">Add to cart</a> 
+         <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/Bosch DIN 74 S4H35 74AH Battery.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Bosch DIN 74 S4H35 74AH Battery</h5>
+    <p class="card-text">Looking for a Bosch DIN 74 S4H35 74AH battery in Kenya? Trust reliable performance with a 74AH rating.
+         Ideal for various vehicles. Ensure compatibility and consult experts for a seamless replacement. Get yours for dependable power.</p>
+    <a href="#" class="btn btn-warning ">Add to cart</a>
+    <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/DUNLOP GRANDTREK AT30 26555R19.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">DUNLOP GRANDTREK AT30 26555R19</h5>
+    <p class="card-text">Searching for DUNLOP GRANDTREK AT30 265/55R19 tires in Kenya? Experience superior on-road and off-road performance. 
+        These tires offer durability and traction for diverse terrains. Confirm compatibility and consider professional advice for the best 
+        tire solution. Enhance your driving experience today!</p>
+    <a href="#" class="btn btn-warning ">Add to cart</a>
+    <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        <div class="col-md-4 mb-2 "   >
+        <div class="card">
+  <img src="images/Smart Car TPMS Tire Pressure Monitoring System Solar Power Digital.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Smart Car TPMS Tire Pressure Monitoring System Solar Power Digital</h5>
+    <p class="card-text">Looking for a Smart Car TPMS (Tire Pressure Monitoring System) with solar power and digital display in Kenya?
+         Ensure optimal tire pressure for safety and efficiency. Solar-powered with a digital interface for convenience. 
+         Enhance your driving experience with advanced technology. Get yours now!</p>
+    <a href="#" class="btn btn-warning">Add to cart</a>
+    <a href="#" class="btn btn-dark">View more</a>
+  </div>
+</div>
+        </div>
+        </div>
+        </div>
+     
+    <div class="col-md-2 bg-dark p-0">
+        <!-- side navigation -->
+        <!-- brands to be displayed -->
+        <ul class="navbar-nav me-auo text-center">
+            <li class="nav-item bg-dark">
+                <a href="#" class="nav-link text-light"><h4>  Brands</h4></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning">  Brand1</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light  bg-warning ">  Brand2</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light  bg-warning">  Brand3</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light  bg-warning">  Brand3</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning">  Brand4</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning">  Brand5</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Brand6</a>
+            </li>
+        </ul>
+        <!-- category to be displayed -->
+        <ul class="navbar-nav me-auo text-center">
+            <li class="nav-item bg-info">
+                <a href="#" class="nav-link text-light bg-dark"><h4> Categories</h4></a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category1</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category2</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category3</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category3</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category4</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category5</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-light bg-warning"> Category6</a>
+            </li>
+        </ul>
+    </div>
+   
+</div>
 <!-- last child -->
 
 <div class="bginfo p-3 text-center">
