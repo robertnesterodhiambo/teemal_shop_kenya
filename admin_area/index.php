aea869f1de35972f94409120a96f41473d8e4f3d
robert nester odhiambo <robertnesterodhiambo@gmail.com>
2023-08-27 10:45:07 +0300

    inserting the get functions

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,6 +8,8 @@
     <title>Admin dashboard</title>
     <!--bootstarp css link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- font awsoment -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <!-- nabar -->
@@ -39,11 +41,11 @@
                     <p class="text-light text-center">Admin Name</p>
                   </div>
                   <div class="button text-center">
-                    <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">Insert products</a></button>
+                    <button class = "px-2"><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert products</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
-                    <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button class = "px-2"><a href="index.php?insert_brands" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
                     <button class = "px-2"><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
@@ -52,6 +54,18 @@
                   </div>
             </div>
            </div>
+
+           <div class="container my-5">
+            <?php
+            if(isset($_GET['insert_category'])){
+                include('insert_categories.php');
+            }
+            if(isset($_GET['insert_brands'])){
+                include('insert_brands.php');
+            }
+            ?>
+           </div>
+           <!--last child-->
            <div class="bg-warning p-3 text-center">
     <p>All rght reserved © Desgned by Robert Nester Odhiambo for TeemalAuto </p>
 </div>
---
+++ admin_area/insert_brands.php
@@ -0,0 +1,15 @@
+<form action = "" method = "post" class = "mb-2">
+<div class="input-group w-90 mb-2">
+  <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+  <input type="text" class="form-control" name="cat_title" placeholder="insert Brands" aria-label="brands" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group w-10 mb-2">
+<!--  <input type="submit" class="form-control bg-info" name="insert_cat"
+ value="insert Brands" placeholder="insert Brands" aria-label="Username"
+  aria-describedby="basic-addon1"> -->
+  <button  class="my-3 bg-info p-3 border-0">insert Brands</button>
+</div>
+
+
+</form>
