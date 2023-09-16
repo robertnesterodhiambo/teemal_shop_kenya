c47af9a4386501978825746dcd856aac6ceea9fd
Idriss <robertnesterodhiambo@gmail.com>
2023-09-16 11:21:59 +0300

    functional search button

--- funcions/common_functions.php
+++ funcions/common_functions.php
@@ -178,4 +178,45 @@ while($row_data = mysqli_fetch_assoc($result_category)){
 }
 }
 
+//searching products 
+ 
+function search_product(){
+  global $con; 
+  if(isset($_GET['search_dataproduct']))
+  {
+     $search_data_value = $_GET['search_data'];
+// getting products information
+$search_query = "select * from products where productt_keywords like '%$search_data_value%'";
+$result_query = mysqli_query($con, $search_query);
+// $row = mysqli_fetch_assoc($result_query);
+// echo $row['product_title'];
+
+$num_of_row = mysqli_num_rows($result_query);
+if($num_of_row == 0) {
+  echo "<h2 class = 'text-center text-danger'>This product is not abailable</h2>";
+}
+
+
+while($row = mysqli_fetch_assoc($result_query)){
+$product_id = $row['product_id'];
+$product_title = $row['product_title'];
+$product_description = $row['product_description'];
+$product_image1 = $row['product_image1'];
+$product_price = $row['prduct_price'];
+$category_id = $row['category_id'];
+$brand_id = $row['brand_id'];
+echo "  <div class='col-md-4 mb-2 '   >
+<div class='card'>
+<img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+<div class='card-body'>
+<h5 class='card-title'> $product_title</h5>
+<p class='card-text'>$product_description</p>
+<a href='#' class='btn btn-warning'>Add to cart</a>
+<a href='#' class='btn btn-dark'>View more</a>
+</div>
+</div>
+
+</div>";
+} }}
+
 ?>
